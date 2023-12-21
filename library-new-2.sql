4b40965035a9ce217237f7ea626562d2115caa28
Ebrahem Alwish <ebrahemk968@gmail.com>
2023-12-22 02:39:58 +0300

    fixDB

--- library-new-2.sql
+++ library-new-2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 21, 2023 at 11:31 PM
+-- Generation Time: Dec 21, 2023 at 11:39 PM
 -- Server version: 5.7.39
 -- PHP Version: 8.2.0
 
@@ -46,6 +46,25 @@ CREATE TABLE `admin` (
 INSERT INTO `admin` (`id`, `name`, `email`, `password`, `age`, `gender`, `status`, `phone`, `img`) VALUES
 (3, 'Maria', 'maria@gmail.com', 'password', 22, 1, 1, '0987654321', 'knsdbfjnsdbfjnds');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher`
+--
+
+CREATE TABLE `teacher` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `age` int(11) NOT NULL,
+  `gender` tinyint(4) NOT NULL,
+  `status` tinyint(4) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `img` varchar(255) DEFAULT NULL,
+  `created_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 --
 -- Indexes for dumped tables
 --
@@ -57,6 +76,13 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `teacher`
+--
+ALTER TABLE `teacher`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `created_by` (`created_by`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -65,6 +91,22 @@ ALTER TABLE `admin`
 --
 ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `teacher`
+--
+ALTER TABLE `teacher`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `teacher`
+--
+ALTER TABLE `teacher`
+  ADD CONSTRAINT `teacher_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `admin` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
